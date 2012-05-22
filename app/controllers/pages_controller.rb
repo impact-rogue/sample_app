fcd08f8dea223a4524eab7fec8f6ee7e52840567
Sergio Montoya LAP <paine9999@hotmail.com>
2012-05-21 20:37:07 -0500

    Agregadas dos paginas mas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,12 @@ class PagesController < ApplicationController
   def contact
   	@title = "Contacto"
   end
+
+  def about
+  	@title = "Sobre nosotros"
+  end
+  
+  def help 
+  	@title = "Ayuda"
+  end
 end
