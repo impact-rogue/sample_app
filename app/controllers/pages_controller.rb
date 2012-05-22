1699c63322249721bdaf6393f1cdfc9a20cf4266
Sergio Montoya LAP <paine9999@hotmail.com>
2012-05-21 20:18:50 -0500

    Commit de todo lo que hice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Principal"
   end
 
   def contact
+  	@title = "Contacto"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'home'" do
+  describe "GET - Para obtener 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
   end
 
-  describe "GET 'contact'" do
+  describe "GET - Para obtener 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
